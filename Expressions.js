9c62947b1edac0f0f8df03324a5a3441cfe18581
AnhTu <huynhcaoanhtu25502.com>
2023-08-14 17:06:22 +0700

    Example for Expressions part

---
+++ Expressions.js
@@ -0,0 +1,10 @@
+let a =true;
+let b =false;
+
+let result_and = a && b;
+let result_or = a || b;
+let result_not = !a;
+
+console.log("Result when a&&b: "+result_and)
+console.log("Result when a||b: "+result_or)
+console.log("Result when !a: "+result_not)
