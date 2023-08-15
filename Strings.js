c8ac679c40651bf16c1ac82cb1996c08dad8afbd
AnhTu <huynhcaoanhtu25502.com>
2023-08-15 17:34:27 +0700

    Example for String part

---
+++ Strings.js
@@ -0,0 +1,11 @@
+
+const name = 'Flavio';
+name.length;
+console.log(name);
+
+const test = 'test';
+const string = `something ${test}`
+console.log(string);
+
+const string1 = `something ${1 + 2 + 3}`
+console.log(string1);
