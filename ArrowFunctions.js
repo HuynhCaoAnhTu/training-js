5b02c5c9551a85e9091192c9c474e22b03a3e06f
AnhTu <huynhcaoanhtu25502.com>
2023-08-16 17:56:15 +0700

    Example for ArrowFunction part

---
+++ ArrowFunctions.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+console.log(add(5, 3))
+
+const square = x => x * x;
+console.log(square(4)); 
+
+const sayHello = () => console.log("Hello!");
+sayHello(); // Kết quả: Hello!
