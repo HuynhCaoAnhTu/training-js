5f0cbe8cd0681c34f3b92175b3a06b5af1c3e796
AnhTu <huynhcaoanhtu25502.com>
2023-08-17 17:38:48 +0700

    Example for Callback part

---
+++ Callback.js
@@ -0,0 +1,28 @@
+// ------Callback Settimeout -------
+function delayedMessage(message, delay, callback) {
+  setTimeout(function () {
+    callback(message);
+  }, delay);
+}
+
+function showMessage(msg) {
+  console.log("Thông báo:", msg);
+}
+
+delayedMessage("Xin chào!", 2000, showMessage);
+
+// ------Callback Create Person -------
+function createPerson(name, age, callback) {
+  const person = {
+    name: name,
+    age: age
+  };
+  callback(person);
+}
+
+function displayPersonInfo(person) {
+  console.log("Tên:", person.name);
+  console.log("Tuổi:", person.age);
+}
+
+createPerson("Alice", 30, displayPersonInfo);
