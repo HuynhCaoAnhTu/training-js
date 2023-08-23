64ed590df5e4725719c368ae7856db79319fda88
AnhTu <huynhcaoanhtu25502.com>
2023-08-23 15:34:13 +0700

    DOM Modifying Attributes

---
+++ attributes.js
@@ -0,0 +1,18 @@
+// Assign image element
+const img = document.querySelector('img');
+
+img.hasAttribute('src');
+img.getAttribute('src');
+img.removeAttribute('src');
+img.setAttribute('src', 'https://img.freepik.com/free-vector/pink-octopus-cartoon-design_1308-107087.jpg?w=2000');
+
+// Select the second div by class name
+// Select the first div
+
+// Select div
+// const div = document.querySelector('.shape');
+
+// div.style.borderRadius = '50%';
+// div.style.display = 'flex';
+// div.style.justifyContent = 'center';
+// div.style.alignItems = 'center';
