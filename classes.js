5873e01c57d0830abffc879445cc7f1ed8e13cb1
AnhTu <huynhcaoanhtu25502.com>
2023-08-23 15:34:47 +0700

    DOM Modifying Classes

---
+++ classes.js
@@ -0,0 +1,5 @@
+const activeDiv = document.querySelector('.active');
+activeDiv.classList.add('hidden');
+activeDiv.classList.remove('hidden');
+activeDiv.classList.toggle('hidden');
+activeDiv.classList.replace('active', 'warning');
