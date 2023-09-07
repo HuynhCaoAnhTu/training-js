34fe052a1b1754dc26ddb8f1cd80d62bf9fa7901
AnhTu <huynhcaoanhtu25502.com>
2023-09-07 20:49:44 +0700

    solution for lab4

---
+++ exercise-two/lab4/index.js
@@ -0,0 +1,8 @@
+function getAttributes() {
+  var infor = document.getElementById('w3r');
+  console.log("href: " + infor.href);
+  console.log("hreflang :" + infor.hreflang);
+  console.log("rel :" + infor.rel);
+  console.log("target :" + infor.target);
+  console.log("type :" + infor.type);
+}
