8cbeec18ee4f15d0fc9c5380e3b038c6589f2e37
AnhTu <huynhcaoanhtu25502.com>
2023-09-29 22:00:49 +0700

    Create Category model

---
+++ javascript-practice/app/javascripts/models/category.js
@@ -0,0 +1,9 @@
+// model.js
+class Category {
+  constructor(id, name, image) {
+    this.categoryId = id
+    this.categoryName = name
+    this.categoryImg = image
+  }
+}
+export default Category;
