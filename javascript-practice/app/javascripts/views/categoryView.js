8c7609679a4e5097718129c19021c3127388ede2
AnhTu <huynhcaoanhtu25502.com>
2023-09-29 22:29:20 +0700

    Update categoryImg to categoryUrl

--- javascript-practice/app/javascripts/views/categoryView.js
+++ javascript-practice/app/javascripts/views/categoryView.js
@@ -14,7 +14,7 @@ class CategoryView {
   renderCategory = (Category) => {
     this.CategoryListEl.innerHTML += `  
     <li class="category-item" category-id="${Category.categoryId}">
-          <img class="category-img" src="${Category.categoryImg}" alt="${Category.categoryName}">
+          <img class="category-img" src="${Category.categoryUrl}" alt="${Category.categoryName}">
           <p class="category-name">${Category.categoryName}</p>
         </li>`;
   }
