b1deebc8b16194bd395b899f5c5658f8be772552
AnhTu <huynhcaoanhtu25502.com>
2023-10-01 22:12:21 +0700

    Implement service for items

---
+++ javascript-practice/app/javascripts/services/itemService.js
@@ -0,0 +1,28 @@
+import LocalStorageService from "./localstorageService";
+import { API_BASE_URL } from "../constants/resoureAPI";
+import { ITEMS_ENDPOINT } from "../constants/resoureAPI";
+class ItemService {
+
+	async getItems() {
+		try {
+			let itemsData = [];
+			const storedData = LocalStorageService.getItemList();
+			if (storedData) {
+				itemsData = JSON.parse(storedData);
+			} else {
+				const response = await fetch(`${API_BASE_URL}${ITEMS_ENDPOINT}`);
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				itemsData = await response.json();
+				LocalStorageService.saveListToStorage('itemsData', itemsData);
+			}
+			return itemsData;
+		} catch (error) {
+			throw error;
+		}
+	}
+
+}
+
+export default ItemService;
