12a01e1ffb4ebdcce2c39d0408bb926a6c72d135
AnhTu <huynhcaoanhtu25502.com>
2023-10-01 22:13:36 +0700

    Implement viewl for items

---
+++ javascript-practice/app/javascripts/views/itemView.js
@@ -0,0 +1,31 @@
+// view.js
+class ItemView {
+
+  constructor() {
+    this.ItemListEl = document.getElementById('items');
+    this.ItemTotal = document.getElementById('menu-total');
+  }
+  renderItemList = (itemList) => {
+    this.ItemTotal.innerHTML= `${itemList.length} result`;
+    this.ItemListEl.innerHTML = "";
+    itemList.forEach(Item => {
+      this.renderItem(Item);
+    })
+  }
+
+  renderItem = (Item) => {
+    this.ItemListEl.innerHTML += `  
+    <li class="item" item-id="${Item.itemId}">
+      <div class="item-container">
+        <img class="item-img" src="${Item.itemUrl}" alt="${Item.itemName}">
+        <div class="item-content">
+          <p class="item-name">${Item.itemName}</p>
+          <p class="item-des">${Item.itemDes}</p>
+          <p class="item-price">&dollar; ${Item.itemPrice}</p>
+        </div>
+      </div>
+      <button class="btn-sencondary" type="button">View detail</button>
+    </li>`;
+  }
+}
+export default ItemView;
