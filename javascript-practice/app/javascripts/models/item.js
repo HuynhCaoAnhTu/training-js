291df376c399514bc1591bed0e4c56430c8594b6
AnhTu <huynhcaoanhtu25502.com>
2023-10-01 22:11:34 +0700

    Implement model for item

---
+++ javascript-practice/app/javascripts/models/item.js
@@ -0,0 +1,13 @@
+class Item {
+  constructor(id, name, des, categoryid , url, price, sugar, ice) {
+    this.itemId = id
+    this.itemName = name
+    this.itemDes = des
+    this.categoryId=categoryid
+    this.itemUrl = url
+    this.itemPrice= price
+    this.itemNoteSugar = sugar
+    this.itemNoteIce = ice
+  }
+}
+export default Item;
