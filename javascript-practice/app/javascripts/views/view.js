3471e557e8cde1b16d0ed9957b17e11fdb377354
AnhTu <huynhcaoanhtu25502.com>
2023-10-01 22:14:02 +0700

    Add itemView to View

--- javascript-practice/app/javascripts/views/view.js
+++ javascript-practice/app/javascripts/views/view.js
@@ -1,7 +1,10 @@
 import CategoryView from "./categoryView";
+
+import ItemView from "./itemView";
 class View {
     constructor() {
         this.categories = new CategoryView;
+        this.items = new ItemView;
     }
 }
 export default View;
