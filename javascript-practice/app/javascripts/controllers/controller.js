957c737006c0bb59f6fc10e5987991e73a4706cd
AnhTu <huynhcaoanhtu25502.com>
2023-10-03 22:00:44 +0700

    Handle event click category and render item

--- javascript-practice/app/javascripts/controllers/controller.js
+++ javascript-practice/app/javascripts/controllers/controller.js
@@ -4,6 +4,10 @@ class AppController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.categories;
+    this.items;
+    this.selectedCategoryId = 1
+    this.selectedCategoryName = "All menu"
   }
   init = async () => {
 
@@ -19,8 +23,8 @@ class AppController {
   }
 
   loadListCategoryList = () => {
-    const categories = this.model.categoryList.getCategoryList();
-    this.view.categories.renderCategoryList(categories);
+    this.categories = this.model.categoryList.getCategoryList();
+    this.view.categories.renderCategoryList(this.categories);
   }
 
   // CONTROLLER ITEM
@@ -30,9 +34,41 @@ class AppController {
   }
 
   loadListItemList = () => {
-    const items = this.model.itemList.getItemList();
-    this.view.items.renderItemList(items);
+    this.items = this.model.itemList.getItemList();
+    this.renderItemListbyCaterogy(this.items);
+
   }
 
+  getCategoryInforOnClick(event) {
+    this.selectedCategoryId = event.currentTarget.getAttribute('category-id');
+    const category = this.categories.find(category => category.categoryId == this.selectedCategoryId);
+    if (category) {
+      this.selectedCategoryName = category.categoryName;
+    } else {
+      console.log("Unknown Category");
+    }
+  }
+
+  renderItemListbyCaterogy(itemList) {
+    const categoryItems = document.querySelectorAll('.category-item');
+    const categoryDefaut = document.querySelector('.category-item');
+    categoryDefaut.classList.add('active');
+    this.view.items.renderItemList(itemList, this.selectedCategoryName);
+    categoryItems.forEach(item => {
+      item.addEventListener('click', (event) => {
+
+        categoryItems.forEach(otherItem => {
+          otherItem.classList.remove('active');
+        });
+        item.classList.add('active');
+
+        this.getCategoryInforOnClick(event)
+        this.items  = itemList.filter(item => item.categoryId == this.selectedCategoryId);
+        console.log(this.items)
+        this.view.items.renderItemList(this.items, this.selectedCategoryName);
+      });
+    });
+    
+  }
 }
 export default AppController;
