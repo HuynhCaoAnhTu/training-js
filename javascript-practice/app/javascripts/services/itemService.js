791c11780984e6c063554e507744705fc26207c3
AnhTu <huynhcaoanhtu25502.com>
2023-10-10 22:41:22 +0700

    Implement itemService by extends baseService

--- javascript-practice/app/javascripts/services/itemService.js
+++ javascript-practice/app/javascripts/services/itemService.js
@@ -1,33 +1,9 @@
-import LocalStorageService from "./localstorageService";
-import { API_BASE_URL, ITEMS_ENDPOINT } from "../constants/api";
-class ItemService {
-
-	getLocalStorage() {
-		return LocalStorageService.get('itemsData');
-	}
-
-	setLocalStorage(data) {
-		return LocalStorageService.set('itemsData', data);
-	}
-
-	async getItems() {
-		try {
-			let itemsData = [];
-			const storedData = this.getLocalStorage();
-			if (storedData) {
-				itemsData = JSON.parse(storedData);
-			} else {
-				const response = await fetch(`${API_BASE_URL}${ITEMS_ENDPOINT}`);
-				if (!response.ok) {
-					throw new Error('Network response was not ok');
-				}
-				itemsData = await response.json();
-				this.setLocalStorage(itemsData);
-			}
-			return itemsData;
-		} catch (error) {
-			throw error;
-		}
+import { ITEMS_ENDPOINT } from "../constants/api";
+import { ITEMS_STORAGE_KEY } from "../constants/key";
+import BaseService from "./baseService";
+class ItemService extends BaseService {
+	constructor(){
+		super(ITEMS_STORAGE_KEY,ITEMS_ENDPOINT)
 	}
 }
 export default ItemService;
