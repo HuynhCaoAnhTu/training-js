5d107787d3097c5113fde4e14a2a051e5ce814eb
AnhTu <huynhcaoanhtu25502.com>
2023-10-10 22:41:03 +0700

    Implement categoryService by extends baseService

--- javascript-practice/app/javascripts/services/categoryService.js
+++ javascript-practice/app/javascripts/services/categoryService.js
@@ -1,33 +1,9 @@
-import LocalStorageService from "./localstorageService";
-import { API_BASE_URL, CATEGORIES_ENDPOINT } from "../constants/api";
-class CategoryService {
-
-	getLocalStorage() {
-		return LocalStorageService.get("categoriesData");
-	}
-
-	setLocalStorage(data) {
-		return LocalStorageService.set("categoriesData", data);
-	}
-
-	async getCategories() {
-		try {
-			let categoriesData = [];
-			const storedData = this.getLocalStorage();
-			if (storedData) {
-				categoriesData = JSON.parse(storedData);
-			} else {
-				const response = await fetch(`${API_BASE_URL}${CATEGORIES_ENDPOINT}`);
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				categoriesData = await response.json();
-				this.setLocalStorage(categoriesData);
-			}
-			return categoriesData;
-		} catch (error) {
-			throw new Error("Failed to get items");
-		}
+import { CATEGORIES_ENDPOINT } from "../constants/api";
+import { CATEGORIES_STORAGE_KEY } from "../constants/key";
+import BaseService from "./baseService";
+class CategoryService extends BaseService {
+	constructor(){
+		super(CATEGORIES_STORAGE_KEY,CATEGORIES_ENDPOINT)
 	}
 }
 export default CategoryService;
