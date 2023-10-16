d85a90bb811037caedd8e50244ce01674c473452
AnhTu <huynhcaoanhtu25502.com>
2023-10-16 21:10:16 +0700

    Add constant storage key for Item in bill

--- javascript-practice/app/javascripts/constants/key.js
+++ javascript-practice/app/javascripts/constants/key.js
@@ -1,3 +1,4 @@
 export const ITEMS_STORAGE_KEY = "itemsData";
 export const CATEGORIES_STORAGE_KEY = "categoriesData";
+export const BILL_STORAGE_KEY = "itemsInBill";
 export const KEY_CODE_ENTER = "13";
