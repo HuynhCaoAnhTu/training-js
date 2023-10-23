c974feb744d73cae14e4977da76524409b8d3a17
AnhTu <huynhcaoanhtu25502.com>
2023-10-23 21:15:19 +0700

    Add ingerdient to model

--- javascript-practice/app/javascripts/models/model.js
+++ javascript-practice/app/javascripts/models/model.js
@@ -1,12 +1,14 @@
 import Bill from "./bill";
 import CategoryList from "./categories";
 import ProductList from "./products";
+import Ingredient from "./product"
 
 class Model {
   constructor() {
     this.categoryList = new CategoryList();
     this.productList = new ProductList();
 		this.bill= new Bill()
+		this.ingerdient= new Ingredient()
   }
 }
 export default Model;
