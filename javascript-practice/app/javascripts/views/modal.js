090b643c68f7c8aa499979abcf903aabb3460049
AnhTu <huynhcaoanhtu25502.com>
2023-10-25 23:09:49 +0700

    Implement render checkout modal

--- javascript-practice/app/javascripts/views/modal.js
+++ javascript-practice/app/javascripts/views/modal.js
@@ -1,19 +1,47 @@
 class ModalView {
 	constructor() {
-		this.modalEl = document.querySelector(".modal");
+		this.tableContent = document.querySelector("#bill-table tbody");
 	}
 
 	openViewModal = (product) => {
 		console.log(product)
 		if (product) {
-			this.modalEl.setAttribute("data-id", product.productId);
-			console.log(this.modalEl.querySelector('.modal-product-image'))
-			this.modalEl.querySelector('.modal-product-image').src = product.productUrl;
-			this.modalEl.querySelector('.modal-product-name').textContent = product.productName;
-			this.modalEl.querySelector('.modal-product-des').textContent = product.productDes;
-			this.modalEl.querySelector('.modal-product-price').innerHTML = `&dollar;${product.productPrice}`;
-			this.modalEl.style.display = 'block'
+			const modalEl = document.getElementById("viewModal");
+			modalEl.setAttribute("data-id", product.productId);
+			modalEl.querySelector('.modal-product-image').src = product.productUrl;
+			modalEl.querySelector('.modal-product-name').textContent = product.productName;
+			modalEl.querySelector('.modal-product-des').textContent = product.productDes;
+			modalEl.querySelector('.modal-product-price').innerHTML = `&dollar;${product.productPrice}`;
+			modalEl.style.display = 'block'
 		};
 	}
+
+	openCheckoutModal = (bill,total) => {
+		console.log(bill)
+		if (bill) {
+			const modalEl = document.getElementById("checkoutModal");
+			const totalBill=document.querySelector(".table-product-total-bill");
+			this.tableContent.innerHTML ="";
+			bill.forEach(product => {
+				this.renderProductPerCheckout(product);
+			})
+			totalBill.innerHTML = `&dollar;${total}`;
+			modalEl.style.display = 'block'
+		};
+	}
+
+	renderProductPerCheckout(product) {
+		const tableContent = document.querySelector("#bill-table tbody");
+		console.log(tableContent);
+		console.log(product);
+
+		this.tableContent.innerHTML +=
+		`<tr>
+			<td class="table-product-name">${product.name}</td>
+			<td class="table-product-quantity">${product.quantity}</td>
+			<td class="table-product-total">&dollar;${product.total}</td>
+		</tr>`;
+	}
+
 }
 export default ModalView;
