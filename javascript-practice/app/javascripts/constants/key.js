58a9a717974e2e0cc31f9eb501d4187676de9cdf
AnhTu <huynhcaoanhtu25502.com>
2023-10-30 21:47:40 +0700

    renameBILL_STORAGE_KEY

--- javascript-practice/app/javascripts/constants/key.js
+++ javascript-practice/app/javascripts/constants/key.js
@@ -1,4 +1,4 @@
 export const PRODUCTS_STORAGE_KEY = "productsData";
 export const CATEGORIES_STORAGE_KEY = "categoriesData";
-export const BILL_STORAGE_KEY = "itemsInBill";
+export const BILL_STORAGE_KEY = "billData";
 export const KEY_CODE_ENTER = "13";
