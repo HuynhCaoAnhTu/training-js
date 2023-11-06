c308dc098164de5d832a4f6bf84d7fc4959694d6
AnhTu <huynhcaoanhtu25502.com>
2023-11-06 13:25:38 +0700

    Update categoryView

--- javascript-practice/app/javascripts/views/categoryView.js
+++ javascript-practice/app/javascripts/views/categoryView.js
@@ -1,30 +1,21 @@
 // view.js
 class CategoryView {
 
-  constructor() {
-    this.CategoryListEl = document.getElementById('categories');
-  }
-  renderCategoryList = (categoryList) => {
-    // this.CategoryListEl.innerHTML = "";
-    categoryList.forEach(Category => {
-      this.renderCategory(Category);
-    })
-  }
+	constructor() {
+		this.CategoryListEl = document.getElementById('categories');
+	}
+	renderCategoryList = (categoryList) => {
+		categoryList.forEach(Category => {
+			this.renderCategory(Category);
+		})
+	}
 
-  renderCategory = (Category) => {
-    if(Category.categoryId==1){
-      this.CategoryListEl.innerHTML += `  
-      <li class="category-item active" category-id="${Category.categoryId}">
-            <img class="category-img" src="${Category.categoryUrl}" alt="${Category.categoryName}">
-            <p class="category-name">${Category.categoryName}</p>
-          </li>`;
-    }
-    else
-    this.CategoryListEl.innerHTML += `  
+	renderCategory = (Category) => {
+		this.CategoryListEl.innerHTML += `
     <li class="category-item" category-id="${Category.categoryId}">
           <img class="category-img" src="${Category.categoryUrl}" alt="${Category.categoryName}">
           <p class="category-name">${Category.categoryName}</p>
         </li>`;
-  }
+	}
 }
 export default CategoryView;
