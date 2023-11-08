8f2c56cdab4f6f06b5fd193ccddc747d86ba5df4
AnhTu <huynhcaoanhtu25502.com>
2023-11-08 21:57:19 +0700

    Remove keycode

--- javascript-practice/app/javascripts/constants/storageKey.js
+++ javascript-practice/app/javascripts/constants/storageKey.js
@@ -2,4 +2,3 @@ export const PRODUCTS_STORAGE_KEY = "productsData";
 export const CATEGORIES_STORAGE_KEY = "categoriesData";
 export const BILL_STORAGE_KEY = "billData";
 export const HISTORYPAYMENT_STORAGE_KEY = "hisroty_payment";
-export const KEY_CODE_ENTER = "13";
