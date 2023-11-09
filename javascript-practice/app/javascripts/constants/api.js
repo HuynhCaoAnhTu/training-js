b5da1160f5a9186d02ff9ea74c94909284b14a5d
AnhTu <huynhcaoanhtu25502.com>
2023-11-09 12:40:00 +0700

    update base url

--- javascript-practice/app/javascripts/constants/api.js
+++ javascript-practice/app/javascripts/constants/api.js
@@ -1,5 +1,5 @@
 //  base URL API
-export const API_BASE_URL = 'practice-js-json-server.vercel.app';
+export const API_BASE_URL = 'https://practice-js-json-server.vercel.app';
 // EndPoint
 export const CATEGORIES_ENDPOINT = '/categories';
 export const PRODUCTS_ENDPOINT = '/products';
